bd4a9a28f046986f67ac8e64a8017b92e878f5a8
Matthew Bracher <matthewbracher@Matthews-MacBook-Pro.local>
2022-09-19 12:15:00 +1000

    commit 2 - week 3

--- app.py
+++ app.py
@@ -1,12 +1,101 @@
-from flask import Flask
-from views import views
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+from unicodedata import name
+from flask import Flask, Blueprint, render_template, request, jsonify, redirect, url_for, session, flash
 
 app = Flask(__name__)
 
+#todo - Make a proper key 
+app.secret_key = "testkey"
+app.config["SQLALCHEMY_DATABASE_URI"] ='sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#Choose a session lifetime - Maybe 10 minutes?
+app.permanent_session_lifetime = timedelta(minutes = 10)
 
-app.register_blueprint(views, url_prefix="/")
+db =  SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/manager",methods=["POST", "GET"])
+def manager():
+    if request.method == "POST":
+        user = request.form["nm"]
+        found_user = users.query.filter_by(name=user).delete()
+        #if found_user:
+            #found_user.delete()
+            
+    return render_template("manager.html", values=users.query.all())
+
+@app.route("/login",methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email            
+        else:
+            usr = users(user, "", "")
+            db.session.add(usr)
+            db.session.commit()
+
+        flash("Login Successful!")
+        return redirect(url_for("user"))
+
+    else:  
+        if "user" in session:
+            flash("You are already logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user", methods=["POST","GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email was saved")
+
+        else:
+            if 'email' in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
+    else:
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}", "info")
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("login"))
+
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, port=8000)
 
 
--- views.py
+++ views.py
@@ -1,75 +1,130 @@
 from re import S
-from flask import Blueprint, render_template, request,jsonify,redirect,url_for
+from unicodedata import name
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for, session, flash
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+import app
+
+db = SQLAlchemy(app)
 
 views = Blueprint(__name__, "views")
 
-def db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect("db.sqlite")
-    except sqlite3.error as e:
-        print(e)
-    return conn
+class users(db.Model):
+    _id = db.Column("id",db.Integer, primary_key = True)
 
-@views.route("/",methods=["GET","POST"])
-def home():
-    conn = db_connection()
-    cursor = conn.cursor()
-    if request.method == "GET":
-        cursor = conn.execute("SELECT * FROM database")
-        test = [
-            dict(id=row[0], username=row[1], password=row[2], website=row[3])
-            for row in cursor.fetchall()
-        ]
-        if test is not None:
-            return jsonify(test)
 
-    if request.method == "POST":
-        new_username = request.form["Username"]
-        new_password = request.form["Password"]
-        new_website = request.form["Website"]
-        sql = """INSERT INTO database (username, password, website)
-            VALUES(?,?,?)"""
 
-        cursor = cursor.execute(sql, (new_username, new_password, new_website))
-        conn.commit()
-        return f"test id {cursor.lastrowid} created successfully "
+# def db_connection():
+#     conn = None
+#     try:
+#         conn = sqlite3.connect("db.sqlite")
+#     except sqlite3.error as e:
+#         print(e)
+#     return conn
 
-    if request.method == "DELETE":
-        sql = """DELETE FROM database WHERE id=? """
-        conn.execute(sql, (id,))
-        conn.commit()
+# @views.route("/",methods=["GET","POST"])
+# def home():
+#     conn = db_connection()
+#     cursor = conn.cursor()
+#     if request.method == "GET":
+#         cursor = conn.execute("SELECT * FROM database")
+#         test = [
+#             dict(id=row[0], username=row[1], password=row[2], website=row[3])
+#             for row in cursor.fetchall()
+#         ]
+#         if test is not None:
+#             return jsonify(test)
+
+#     if request.method == "POST":
+#         new_username = request.form["Username"]
+#         new_password = request.form["Password"]
+#         new_website = request.form["Website"]
+#         sql = """INSERT INTO database (username, password, website)
+#             VALUES(?,?,?)"""
+
+#         cursor = cursor.execute(sql, (new_username, new_password, new_website))
+#         conn.commit()
+#         return f"test id {cursor.lastrowid} created successfully "
+
+#     if request.method == "DELETE":
+#         sql = """DELETE FROM database WHERE id=? """
+#         conn.execute(sql, (id,))
+#         conn.commit()
     
-    return render_template("index.html", name = "Matt")
+#     return render_template("index.html", name = "Matt")
 
-    
-
-@views.route("/profile")
+@views.route("/manager")
 def profile():
-    return render_template("profile.html")
+    return render_template("manager.html")
 
-@views.route("/json")
-def get_json():
-    return jsonify({'name':'tim','coolness':10})
+@views.route("login",methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login Successful!")
+        return redirect(url_for("views.user"))
 
-@views.route("/data")
-def get_data():
-    data = request.json
-    return jsonify(data)
+    else:  
+        if "user" in session:
+            flash("You are already logged in!")
+            return redirect(url_for("views.user"))
+        return render_template("login.html")
 
-@views.route("/go-to-home")
-def go_to_home():
-    return redirect(url_for("views.home"))
+@views.route("/user", methods=["POST","GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
 
-@views.route('/addRegion', methods=['POST'])
-def addRegion():
-    return (request.form['Username'])
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email was saved")
+
+        else:
+            if 'email' in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
+    else:
+        return redirect(url_for("views.login"))
+
+@views.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        flash(f"You have been logged out, {user}", "info")
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("views.login"))
+    
+
+# @views.route("/profile")
+# def profile():
+#     return render_template("profile.html")
+
+# @views.route("/json")
+# def get_json():
+#     return jsonify({'name':'tim','coolness':10})
+
+# @views.route("/data")
+# def get_data():
+#     data = request.json
+#     return jsonify(data)
+
+# @views.route("/go-to-home")
+# def go_to_home():
+#     return redirect(url_for("views.home"))
+
+# @views.route('/addRegion', methods=['POST'])
+# def addRegion():
+#     return (request.form['Username'])
 
 
-@views.route('/delete/<int:id>')
-def delete(id):
-    return render_template("index.html", name = "Matt")
+# @views.route('/delete/<int:id>')
+# def delete(id):
+#     return render_template("index.html", name = "Matt")
 
 
 
